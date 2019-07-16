c0697f65fdb38c2798981bcf1f8b6f6b9efd0b86
Alex <rylatgl@gmail.com>
2019-07-16 16:17:38 +0200

    Rewrite pull-blob to pull a single file

--- pull-blobs.py
+++ pull-blobs.py
@@ -1,15 +1,25 @@
 import os, sys
 from thirdparty.googleplayapi.gpapi.googleplay import GooglePlayAPI
 
+if len(sys.argv) <= 1:
+    print("Usage: pull-blobs.py BLOB_TYPE")
+    sys.exit(1)
+
+arg_blobType = sys.argv[1]
 if not 'gsfDeviceId' in os.environ and not 'deviceCodename' in os.environ and not 'authSubToken' in os.environ:
     print("Some of these environmental variables are missing: gsfDeviceId, deviceCodename, authSubToken")
-    sys.exit(1)
+    sys.exit(2)
 
 gapi = GooglePlayAPI(locale="en_US", timezone="UTC", device_codename=os.environ['deviceCodename'])
 gapi.login(gsfId=int(os.environ['gsfDeviceId']), authSubToken=os.environ['authSubToken'])
 download_result = gapi.download('com.studiowildcard.wardrumstudios.ark', expansion_files = True)
 for data_info in download_result['additionalData']:
     print(data_info["type"], data_info["versionCode"])
-    with open(".".join([data_info["type"], str(data_info["versionCode"]), "bin"]), "wb") as file_handle:
+    if data_info["type"] != arg_blobType:
+        print("skipping")
+        continue
+    
+    print("fetching")
+    with open(".".join([data_info["type"], str(data_info["versionCode"]), "blob"]), "wb") as file_handle:
         for chunk in data_info["file"]["data"]:
             file_handle.write(chunk)
