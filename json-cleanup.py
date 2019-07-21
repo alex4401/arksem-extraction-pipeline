0031af2b4afb15f5ac95568f16cd338fddcf7122
Alex <rylatgl@gmail.com>
2019-07-21 14:37:39 +0200

    remove empty properties from JSONs

--- json-cleanup.py
+++ json-cleanup.py
@@ -2,6 +2,10 @@ import sys
 import json
 from collections import OrderedDict
 
+IGNORED_EMPTY_PROPERTIES = [
+    "BoolProperty", "ObjectProperty", "FloatProperty", "Function"
+]
+
 def make_unique(key, dct):
     counter = 0
     unique_key = key
@@ -14,14 +18,28 @@ def make_unique(key, dct):
 def parse_object_pairs(pairs):
     dct = OrderedDict()
     for key, value in pairs:
+        if key == 'export_type' \
+            and type(value) is str \
+            and value in IGNORED_EMPTY_PROPERTIES:
+            continue
         if key in dct:
             key = make_unique(key, dct)
         dct[key] = value
-
     return dct
+
+def clean_final_object(arr):
+    final_arr = []
+    for index in range(len(arr)):
+        obj = arr[index]
+        if type(obj) is OrderedDict \
+            and len(obj) == 0:
+            continue
+        final_arr.append(obj)
+    return final_arr
 
 decoder = json.JSONDecoder(object_pairs_hook=parse_object_pairs)
 with open(sys.argv[1], 'r') as file_handle:
     bp_data = decoder.decode(file_handle.read())
+    bp_data = clean_final_object(bp_data)
     with open(sys.argv[2], 'w') as file_handle:
         file_handle.write(json.dumps(bp_data, indent=2, sort_keys=True))
