2da0c45619234a9b1b4101ab33b6342171242634
alex4401 <rylatgl@gmail.com>
2019-10-27 10:35:34 +0100

    Run a formatter on gptool.env

--- gptool/env.py
+++ gptool/env.py
@@ -1,10 +1,13 @@
-import os, sys
+import os
+import sys
 
-package_name: str = os.environ.get('gpAppPackageName') # com.studiowildcard.wardrumstudios.ark
+package_name: str = os.environ.get('gpAppPackageName')
 device_id: int = int(os.environ.get('gsfDeviceId', 0))
 device_codename: str = os.environ.get('gsfDeviceCodename')
 auth_token: str = os.environ.get('gsfAuthToken')
 
 if device_id == 0 or not device_codename or not auth_token or not package_name:
-    print("Some of these environmental variables are missing: gsfDeviceId, gsfDeviceCodename, gsfAuthToken, gpAppPackageName")
+    print(
+        "Some of these environmental variables are missing: gsfDeviceId, gsfDeviceCodename, gsfAuthToken, gpAppPackageName"
+    )
     sys.exit(2)
