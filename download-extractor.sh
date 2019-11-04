80d4d09b83b6f53c5e0ab2b200f71a17c08952d1
alex4401 <rylatgl@gmail.com>
2019-11-04 21:25:50 +0100

    Rewrite UDE download script to use GitHub Releases

--- download-extractor.sh
+++ download-extractor.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-PROJECT_ID=13304753
-PROJECT_BRANCH=$1
-OUTPUT_FILENAME=$2
+if [ -f "$1/UDE_VERSION" && -f "$1/ude-debug.arm" ]
+then
+    if [ `cat "$1/UDE_VERSION"` == $UDE_VERSION ]
+    then
+        echo "No need to download the extractor: v$UDE_VERSION is already present at the location."
+        exit 0
+    fi
+fi
 
-error_wusage() {
-    echo "error: $@"
-    echo "usage: bash $0 \$PROJECT_BRANCH \$OUTPUT_FILENAME"
-    exit 1
-}
-
-[ -z "$PROJECT_BRANCH" ] \
-    && error_wusage "Extractor branch has not been specified."
-[ -z "$OUTPUT_FILENAME" ] \
-    && error_wusage "Output filename has not been specified."
-
-GITLAB_API="https://gitlab.com/api/v4/projects"
-ARTIFACTS="jobs/artifacts"
-
-FULL_URL="$GITLAB_API/$PROJECT_ID/$ARTIFACTS/$PROJECT_BRANCH/raw/target/debug/uasset-data-extractor?job=build"
-echo "(downloading an artifact: $FULL_URL)"
-exec curl "$FULL_URL" --output "$OUTPUT_FILENAME"
+curl https://github.com/alex4401/ude/releases/download/$UDE_VERSION/ude-debug.arm --output "$1/ude-debug.arm"
+echo $UDE_VERSION > "$1/UDE_VERSION"
